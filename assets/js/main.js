433a3c6add03c0b84da8717a05285fb1eea881b9
Bhabishya Shrestha <bhabisyas@gmail.com>
2024-01-13 17:41:04 -0600

    add js function to integrate filters tab functionality

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,18 @@
 /*=============== FILTERS TABS ===============*/
+const tabs = document.querySelectorAll("[data-target]"),
+  tabContents = document.querySelectorAll("[data-content]");
+
+tabs.forEach((tab) => {
+  tab.addEventListener("click", () => {
+    const target = document.querySelector(tab.dataset.target);
+
+    tabContents.forEach((tc) => {
+      /*tc = tabcontent*/
+      tc.classList.remove("filters__active");
+    });
+    target.classList.add("filters__active");
+  });
+});
 
 /*=============== DARK LIGHT THEME ===============*/
 
