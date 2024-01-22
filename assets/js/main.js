55627d5f4b692aeb93256a463305e5fb1378a625
Bhabishya Shrestha <bhabisyas@gmail.com>
2024-01-22 15:42:14 -0600

    implement scroll animation and change theme

--- assets/js/main.js
+++ assets/js/main.js
@@ -51,3 +51,27 @@ themeButton.addEventListener('click', () => {
   localStorage.setItem('selected-icon', getCurrentIcon())
 })
 /*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+  origin: 'top',
+  distance: '60px',
+  duration: 2500,
+  delay: 400,
+})
+
+sr.reveal(`.profile__border`)
+sr.reveal(`.profile__name`, {delay: 500})
+sr.reveal(`.profile__profession`, {delay: 600})
+sr.reveal(`.profile__social`, {delay: 700})
+sr.reveal(`.profile__info-group`, {interval: 100, delay: 700})
+sr.reveal(`.profile__buttons`, {delay: 800})
+sr.reveal(`.filters__content`, {delay: 900})
+sr.reveal(`.filters`, {delay: 1000})
+
+const reverseSr =  ScrollReveal({
+  origin: 'bottom',
+  distance: '60px',
+  duration: 2500,
+  delay: 400,
+})
+
+sr.reveal(`footer__copy`, {delay: 500})
