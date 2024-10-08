fab7743699574b47ccdbdd20c2d57426ccfa0875
Bhabishya Shrestha <bhabisyas@gmail.com>
2024-10-07 22:24:51 -0500

    implement about me section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "./sections/Navbar";
 import Hero from "./sections/Hero";
+import About from "./sections/About";
 
 const App = () => {
   return (
-    <main className="w-full mx-auto">
+    <main className="max-w-7xl w-full mx-auto relative">
       <Navbar />
       <Hero />
+      <About />
     </main>
   );
 };
